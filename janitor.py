fc245ff17fe0a0114651f8fde88fa7a4036ac796
TobyAM <toby@tobyam.com>
2024-12-20 12:32:26 -0800

    Update janitor.py
    replaced /n with $ in the regex expressions for Time: and Location:

--- janitor.py
+++ janitor.py
@@ -63,8 +63,8 @@ class Janitor:
         if "location" not in location_statement or "time" not in location_statement:
             return LocationStatementState.MISSING
         try:
-            location = re.search(r'location: *(.*)\n', location_statement, re.IGNORECASE).group(1)
-            time_seen = re.search(r'time: *(.*)\n', location_statement, re.IGNORECASE).group(1)
+            location = re.search(r'location: *(.*)$', location_statement, re.IGNORECASE).group(1)
+            time_seen = re.search(r'time: *(.*)$', location_statement, re.IGNORECASE).group(1)
         except Exception as e:
             print(f"Exception {e} during keyword parsing. Marking invalid.")
             return LocationStatementState.INVALID
@@ -125,8 +125,8 @@ class Janitor:
             # and 'saved' content does not need further processing
             self.reddit_handler.save_content(post.submission)
             print("\tPost has valid location statement")
-            location = re.search(r'location: *(.*)\n', location_statement, re.IGNORECASE).group(1)
-            time_seen = re.search(r'time: *(.*)\n', location_statement, re.IGNORECASE).group(1)
+            location = re.search(r'location: *(.*)$', location_statement, re.IGNORECASE).group(1)
+            time_seen = re.search(r'time: *(.*)$', location_statement, re.IGNORECASE).group(1)
             dt_utc = datetime.utcfromtimestamp(post.submission.created_utc)
             formatted_dt = dt_utc.isoformat().replace('T', ' ')
             sheet_values = [[location, time_seen, formatted_dt, f"https://www.reddit.com{post.submission.permalink}"]]
